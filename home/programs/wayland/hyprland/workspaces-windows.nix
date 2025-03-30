926f732c4132fa38d615d295e57c3146d4885f89
kamusari713 <ovsyannikovkostyan@gmail.com>
2025-03-30 13:18:23 +0400

    fix: literatures

--- home/programs/wayland/hyprland/workspaces-windows.nix
+++ home/programs/wayland/hyprland/workspaces-windows.nix
@@ -19,9 +19,9 @@
         "center, class:(spotify)"
         "size, 1000 700, class:(spotify)"
 
-        # Freesmlauncher
-        "workspace 3, class:(Freesmlauncher)$"
-        "float, class:(Freesmlauncher)$"
+        # FreesmLauncher
+        "workspace 3, class:(FreesmLauncher)$"
+        "float, class:(FreesmLauncher)$"
 
         # Telegram
         "workspace 5 silent, class:(TelegramDesktop)$"
