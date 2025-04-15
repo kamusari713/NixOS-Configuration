33c8726d6a30c019f5a7058d5f6c9602252ad448
kamusari713 <ovsyannikovkostyan@gmail.com>
2025-04-15 17:01:22 +0400

    fix: updated nixvim api for ts tools plugin 🧰

--- home/nixvim/plugins/typescript-tools.nix
+++ home/nixvim/plugins/typescript-tools.nix
@@ -2,15 +2,15 @@
   programs.nixvim.plugins.typescript-tools = {
     enable = true;
     settings = {
-      tsserverFilePreferences = {
-        includeInlayParameterNameHints = "all";
-        includeInlayParameterNameHintsWhenArgumentMatchesName = true;
-        includeInlayFunctionParameterTypeHints = true;
-        includeInlayVariableTypeHints = true;
-        includeInlayVariableTypeHintsWhenTypeMatchesName = true;
-        includeInlayPropertyDeclarationTypeHints = true;
-        includeInlayFunctionLikeReturnTypeHints = true;
-        includeInlayEnumMemberValueHints = true;
+      tsserver_file_preferences = {
+        include_inlay_parameter_name_hints = "all";
+        include_inlay_parameter_name_hints_when_argument_matches_name = true;
+        include_inlay_function_parameter_type_hints = true;
+        include_inlay_variable_type_hints = true;
+        include_inlay_variable_type_hints_when_type_matches_name = true;
+        include_inlay_property_declaration_type_hints = true;
+        include_inlay_function_like_return_type_hints = true;
+        include_inlay_enum_member_value_hints = true;
       };
     };
   };
